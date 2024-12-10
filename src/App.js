0663e76cf420b32f61ed6a4af6200cd3443a7982
Carolina Alves <100949929+carolina13as@users.noreply.github.com>
2024-12-10 17:01:54 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, meu nome é Carolina Alves de Souza</h1>
+    </div>
+  );
+
+
+
+  
+}
+
+export default App;
